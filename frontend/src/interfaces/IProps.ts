9289728f643f128004f7a88336c1755065fcb595
Duarte Dias <me@duartedias.me>
2023-04-15 19:29:10 +0100

    Card component

--- frontend/src/interfaces/IProps.ts
+++ frontend/src/interfaces/IProps.ts
@@ -1,4 +1,13 @@
+import internal from "stream"
+
 export interface MainProps{
     children: React.ReactNode
     title: string
+}
+export interface CardProps{
+    title: string
+    image: string
+    price: number
+    discount: number
+    unit: string
 }
