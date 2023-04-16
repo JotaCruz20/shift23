cc934c993eb12f60ffa68342130b8177128871a3
Duarte Dias <me@duartedias.me>
2023-04-16 11:41:20 +0100

    Minor Bug Fixes

--- frontend/src/components/ProdTypeButton.tsx
+++ frontend/src/components/ProdTypeButton.tsx
@@ -5,7 +5,7 @@ import { ProdTypeButtonProps } from "../interfaces/IProps";
 const ProdTypeButton = ({name, selected}: ProdTypeButtonProps) =>{
     
     return(
-        <div className={selected ? "bg-green2 text-white rounded-2xl py-1 px-2" : "bg-white border-green1 border-2 rounded-2xl py-1 px-2" }>
+        <div className={selected ? "bg-green2 text-white rounded-2xl py-1 px-2 hover:cursor-pointer" : "bg-white border-green1 border-2 rounded-2xl py-1 px-2 hover:cursor-pointer" }>
             <p>{name}</p>
         </div>
     )
